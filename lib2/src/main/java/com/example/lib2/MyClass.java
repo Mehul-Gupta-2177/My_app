7391949621b11c1f9db62beb1c3a44baafaa6338
Mehul-Gupta-2177 <mehul.gupta@tufts.edu>
2021-07-08 15:31:21 +0530

    int 2

--- lib2/src/main/java/com/example/lib2/MyClass.java
+++ lib2/src/main/java/com/example/lib2/MyClass.java
@@ -2,6 +2,20 @@ package com.example.lib2;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello Worldy");
+
+        // prints out the name
+        String name = "My name is Mehul";
+        System.out.println(name);
+        System.out.print("\n");
+        // for loop to print out a stair of stars
+        for (int i = 0; i < 10; i++){
+            for (int k = 0; k < i ; k++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
     }
+
+    private String name;
 }
