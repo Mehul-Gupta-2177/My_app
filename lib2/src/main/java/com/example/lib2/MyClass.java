e16c993647b384a2456590ecb325980eebe9aa85
Mehul-Gupta-2177 <mehul.gupta@tufts.edu>
2021-07-08 12:27:49 +0530

    int 2

--- lib2/src/main/java/com/example/lib2/MyClass.java
+++ lib2/src/main/java/com/example/lib2/MyClass.java
@@ -2,7 +2,6 @@ package com.example.lib2;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
+        System.out.println("Hello Worldy");
     }
 }
